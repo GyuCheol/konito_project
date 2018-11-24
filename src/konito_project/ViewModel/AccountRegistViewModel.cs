f7be54f9069616325a7e5ad790480ac3a38b577b
GyuCheol Lee <rbcjf0219@gmail.com>
2018-11-24 10:04:28 +0900

    edit account regist

---
+++ src/konito_project/ViewModel/AccountRegistViewModel.cs
@@ -0,0 +1,19 @@
+﻿using konito_project.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konito_project.ViewModel {
+    public class AccountRegistViewModel {
+        public ObservableCollection<Account> PurchaseList { get; private set; } = new ObservableCollection<Account>();
+        public ObservableCollection<Account> SalesList { get; private set; } = new ObservableCollection<Account>();
+
+        public AccountRegistViewModel() {
+
+        }
+
+    }
+}
